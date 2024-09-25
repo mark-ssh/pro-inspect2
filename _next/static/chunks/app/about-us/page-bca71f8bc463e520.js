0d63296bc1a3231e8b5b1410f6279982dc89f0c5
Mark Wonah <markwonah@gmail.com>
2024-09-25 14:58:21 +0100

    new project

---
+++ _next/static/chunks/app/about-us/page-bca71f8bc463e520.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[554],{1765:function(e,n,s){Promise.resolve().then(s.bind(s,7930)),Promise.resolve().then(s.bind(s,5200)),Promise.resolve().then(s.bind(s,6417)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[51,240,699,422,501,687,357,971,23,744],function(){return e(e.s=1765)}),_N_E=e.O()}]);
